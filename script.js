3cc77bfb9fdf85bae6704eeae351b4a5a3add197
Jeremy Hatfield <jhatfieldkc@gmail.com>
2021-05-25 17:13:39 -0500

    Part 2 Final Commit

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var counts = document.querySelector("#count");
-var counter = 1;
+var countsNeil = document.querySelector("#countNeil");
+var counterNeil = 10;
+var countsNichole = document.querySelector("#countNichole");
+var counterNichole = 13;
+var countsJim = document.querySelector("#countJim");
+var counterJim = 10;
 
-function like() {
-  if (counter == 1) {
-    counts.innerText = counter++ + " Like";
-  } else {
-    {
-      counts.innerText = counter++ + " Likes";
-    }
-  }
+function likeNeil() {
+  countsNeil.innerText = counterNeil++ + " Likes";
+}
+
+function likeNichole() {
+  countsNichole.innerText = counterNichole++ + " Likes";
+}
+
+function likeJim() {
+  countsJim.innerText = counterJim++ + " Likes";
 }
